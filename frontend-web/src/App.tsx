54dea738045f7e082114821947c8160453221391
Alex3997 <alex.devops3997@gmail.com>
2021-03-16 14:42:58 -0300

    Adding title.{id}

--- frontend-web/src/App.tsx
+++ frontend-web/src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
+import { Helmet } from 'react-helmet';
 import Routes from './Routes';
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>Made by: Alex3397</title>
+      </Helmet>
       <Routes />
       <ToastContainer />
     </>
